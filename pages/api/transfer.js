272af27d4948d9c4ab9330cd785b655fdb82be16
Martin Adams <heymartinadams@users.noreply.github.com>
2020-08-10 21:35:09 -0700

    [wip] Article transfer

--- pages/api/transfer.js
+++ pages/api/transfer.js
@@ -1,8 +1,6 @@
 import Webflow from 'webflow-api'
 
 export default async (req, res) => {
-	console.log(process.env.WEBFLOW_API)
-
 	const webflow = new Webflow({ token: process.env.WEBFLOW_API })
 
 	const ids = {
@@ -19,11 +17,18 @@ export default async (req, res) => {
 	const articleItems = await webflow.items({
 		collectionId: ids.collections.articles
 	})
-	articleItems.items.forEach(x => console.log(x['_id'], x['name']))
+	// articleItems.items.forEach(x => console.log(x['_id'], x['name']))
 
 	// Cycle through collection to get each item ID
 	const tagItems = await webflow.items({ collectionId: ids.collections.tags })
 	// tagItems.items.forEach(x => console.log(x['_id'], x['name']))
+
+	const sheet = await fetch(
+		`https://sheets.googleapis.com/v4/spreadsheets/1208NK6XyW2omFE13w6Blm-ACVqU1nxBSk577S0207Uk?key=${process.env.GOOGLE_SHEETS_API}`
+	)
+	const response = await sheet.json()
+
+	console.log(response)
 
 	res.json({ done: true })
 }
