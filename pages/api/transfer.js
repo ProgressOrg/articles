98fab1e82a635ee56841e7972f22f06412a8d805
Martin Adams <heymartinadams@users.noreply.github.com>
2020-08-10 23:14:54 -0700

    [feat] Article transfer API

--- pages/api/transfer.js
+++ pages/api/transfer.js
@@ -13,22 +13,68 @@ export default async (req, res) => {
 		}
 	}
 
+	let webflowItems
+
 	// Cycle through collection to get each field ID
-	const articleItems = await webflow.items({
-		collectionId: ids.collections.articles
+	webflowItems = await webflow.items({
+		collectionId: ids.collections.tags
 	})
-	// articleItems.items.forEach(x => console.log(x['_id'], x['name']))
+	const hashtags = webflowItems.items
 
-	// Cycle through collection to get each item ID
-	const tagItems = await webflow.items({ collectionId: ids.collections.tags })
-	// tagItems.items.forEach(x => console.log(x['_id'], x['name']))
+	webflowItems = await webflow.items({
+		collectionId: ids.collections.categories
+	})
+	const categories = webflowItems.items
 
-	const sheet = await fetch(
-		`https://sheets.googleapis.com/v4/spreadsheets/1208NK6XyW2omFE13w6Blm-ACVqU1nxBSk577S0207Uk?key=${process.env.GOOGLE_SHEETS_API}`
+	/**
+	 * STEP 1: Get spreadsheet data
+	 */
+
+	const rows = 'B151:U186'
+	const gapi = await fetch(
+		`https://sheets.googleapis.com/v4/spreadsheets/1208NK6XyW2omFE13w6Blm-ACVqU1nxBSk577S0207Uk/values/Articles!${rows}?key=${process.env.GOOGLE_SHEETS_API}`
 	)
-	const response = await sheet.json()
+	const response = await gapi.json()
+	// console.log(response.values)
 
-	console.log(response)
+	/**
+	 * STEP 2: Create new articles
+	 */
+
+	for await (let row of response.values) {
+		// 0 title, 1 slug, 2 date, 3 datetime, 4 why, 5 HTML, 6 image, 7 category, 8-16 tags
+
+		// Add hashtags
+		let tags = []
+		for (let i = 8; i < row.length; i++) {
+			if (hashtags.some(x => x.name === row[i])) {
+				tags = [...tags, hashtags.find(x => x.name === row[i])['_id']]
+			}
+		}
+
+		let category = ''
+		if (categories.some(x => x.name === row[7])) {
+			category = categories.find(x => x.name === row[7])['_id']
+		}
+
+		const item = await webflow.createItem({
+			collectionId: ids.collections.articles,
+			fields: {
+				_archived: false,
+				_draft: false,
+				author: '56b8f744075f9ea44d2f036f',
+				category,
+				date: row[3],
+				name: row[0],
+				slug: row[1].replace(/[\W]+/g, ''),
+				tags,
+				text: row[5],
+				'why-it-matters': row[4]
+			}
+		})
+
+		console.log(item)
+	}
 
 	res.json({ done: true })
 }
